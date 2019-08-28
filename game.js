93220b9aa3707bcf0c80c17b13466e782a0feccc
Michał <m.wiczk@wp.pl>
2019-08-28 21:35:45 +0200

    some changes

--- game.js
+++ game.js
@@ -5,21 +5,21 @@ const box = document.querySelector('.box');
 const game_box = document.querySelector('.game_box');
 
 play_button.addEventListener('click', () => {
-    box.classList.add('visually_hide');
-    setTimeout(() => {
-        box.classList.add('hide');
-    }, 1000);
-    setTimeout(() => {
-        game_box.classList.add('appear');
-    }, 1000);
-    setTimeout(() => {
-        game_box.classList.add('visually_appear');
-    }, 1500);
+  box.classList.add('visually_hide');
+  setTimeout(() => {
+    box.classList.add('hide');
+  }, 1000);
+  setTimeout(() => {
+    game_box.classList.add('appear');
+  }, 1000);
+  setTimeout(() => {
+    game_box.classList.add('visually_appear');
+  }, 1500);
 });
 
 back_button.addEventListener('click', () => {
-    box.classList.remove('visually_hide');
-    box.classList.remove('hide');
-    game_box.classList.remove('visually_appear');
-    game_box.classList.remove('appear');
+  box.classList.remove('visually_hide');
+  box.classList.remove('hide');
+  game_box.classList.remove('visually_appear');
+  game_box.classList.remove('appear');
 });
